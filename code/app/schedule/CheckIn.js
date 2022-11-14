5f617ffa41607b33929f6ef6d4b6195c55bc2c9e
jhao <384716045@qq.com>
2022-11-14 19:02:13 +0800

    test

--- code/app/schedule/CheckIn.js
+++ code/app/schedule/CheckIn.js
@@ -1,13 +1,26 @@
 module.exports = {
-    schedule: {
-        interval: '30s',
-        type: 'all'
-    },
-
-    async task() {
-        const result = await ctx.curl('https://sctapi.ftqq.com/SCT182258TRszwFQh1XwoZqmol4Utd9Fia.send?title=messagetitle&desp=messagecontent');
-        ctx.status = result.status;
-        ctx.set(result.headers);
-        ctx.body = result.data;
-    }
-}
+  schedule: {
+    interval: '30m', 
+    type: 'all', // 指定所有的 worker 都需要执行
+    immediate:true
+  },
+  async task(ctx) {
+    const result = await ctx.curl('https://sci.ywtz.top/user/checkin', {
+      method: 'POST',
+      headers: {
+        'cookie': 'uid=2588; email=384716045%40qq.com; key=7eb3392b6f6e602bbbc7c9e2d344ac44c9cfaa7fe9aab; ip=290103da8510183f3e378f401a1053b7; expire_in=1668509163',
+      },
+      dataType: 'json',
+    });
+    await ctx.curl('https://sctapi.ftqq.com/SCT182258TRszwFQh1XwoZqmol4Utd9Fia.send', {
+      method: 'POST',
+      contentType: 'json',
+      data: {
+        title: 'Hello World',
+        desp: result,
+        short:'消息卡片内容'
+      },
+      dataType: 'json',
+    });
+  },
+};
