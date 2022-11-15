0ad076578b2de2fc2f1510c68fd844d4f242e2ff
jhao <384716045@qq.com>
2022-11-15 19:41:58 +0800

    v1.0.0 Test

--- code/app/schedule/CheckIn.js
+++ code/app/schedule/CheckIn.js
@@ -1,26 +1,36 @@
 module.exports = {
-  schedule: {
-    interval: '30m', 
-    type: 'all', // 指定所有的 worker 都需要执行
-    immediate:true
-  },
-  async task(ctx) {
-    const result = await ctx.curl('https://sci.ywtz.top/user/checkin', {
-      method: 'POST',
-      headers: {
-        'cookie': 'uid=2588; email=384716045%40qq.com; key=7eb3392b6f6e602bbbc7c9e2d344ac44c9cfaa7fe9aab; ip=290103da8510183f3e378f401a1053b7; expire_in=1668509163',
-      },
-      dataType: 'json',
-    });
-    await ctx.curl('https://sctapi.ftqq.com/SCT182258TRszwFQh1XwoZqmol4Utd9Fia.send', {
-      method: 'POST',
-      contentType: 'json',
-      data: {
-        title: 'Hello World',
-        desp: result,
-        short:'消息卡片内容'
-      },
-      dataType: 'json',
-    });
-  },
+    schedule: {
+        cron: '0 0 24 * * ?',
+        type: 'all', 
+        immediate: true
+    },
+    async task(ctx) {
+        const result = await ctx.curl('https://alterssc.online/auth/login', {
+            method: 'post',
+            data: {
+                email: '384716045@qq.com',
+                passwd: 'JHSQH3271307'
+            },
+            dataType: 'json'
+        });
+
+        if (result.status == 200) {
+            console.log('登录成功');
+            const checkin = await ctx.curl('https://alterssc.online/user/checkin', {
+                method: 'POST',
+                headers: {
+                    'cookie': result.headers['set-cookie']
+                },
+                dataType: 'json'
+            });
+
+            await ctx.curl('https://sctapi.ftqq.com/SCT182258TRszwFQh1XwoZqmol4Utd9Fia.send',{
+                method:'POST',
+                data: {
+                    title: checkin.data.msg,
+                    desp: checkin.data
+                }
+            })
+        }
+    },
 };
